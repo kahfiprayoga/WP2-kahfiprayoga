fdca7bcd3cc2ff48d59cc79cd7d7c10aa4cd9def
kahfiprayoga <kahfiprayoga8@gmail.com>
2022-03-26 11:37:23 +0700

    menambahkan Latihan1.php, Model_latihan1.php, view-latihan.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Muhammad Kahfi Prayoga
+                saya tinggal di Dramaga Bogor
+                hobi saya adalah bermain alat musik";
+    }
+}
